0418347cce8be0631733f58b09fe693f5b251ee7
Ahmad Abusaif <aabusaif8@gmail.com>
2023-11-02 10:43:41 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
+const pastesRouter = require("./pastes/pastes.router")
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+app.use(express.json())
+
+// app.use("/pastes/:pasteId", (req,res,next) =>{
+//   const { pasteId } = req.params
+//   const foundPaste = pastes.find((paste) =>paste.id === Number(pasteId))
+
+//   if(foundPaste) {
+//     res.json({data:foundPaste})
+//   }else {
+//     next({
+//       status: 404,
+//       message: `Paste id not found: ${pasteId}`
+//     })
+//   }
+// })
+
+const pastes = require("./data/pastes-data")
+
+app.use("/pastes", pastesRouter)
 
 // Not found handler
 app.use((request, response, next) => {
@@ -9,9 +28,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "something went wrong"} = error
+  res.status(status).json({ error: message })
 });
 
 module.exports = app;
